39e54cdfecc3791fc41fda476c98cf4f34733f0c
Shivani Awasthi <shivaniawasthi68@gmail.com>
2026-04-25 13:57:16 +0530

    chore: comment out NewsletterSection and DisclaimerSection in CompanyDetailsPage for future implementation

--- src/app/company-details/page.tsx
+++ src/app/company-details/page.tsx
@@ -44,8 +44,8 @@ export default async function CompanyDetailsPage() {
         </div>
       </CompanyDetailsLayout>
 
-      <NewsletterSection data={data.newsletter} />
-      <DisclaimerSection data={data.disclaimer} />
+      {/* <NewsletterSection data={data.newsletter} /> */}
+      {/* <DisclaimerSection data={data.disclaimer} /> */}
     </>
   );
 }
